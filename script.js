7a1d0d9268d22356bffe7a5e3b382ae57ae8303b
Dmitry <dmitri.designer@gmail.com>
2023-06-01 15:23:03 +0300

    урок №2

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+const title = "строка с названием проекта";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 5;
+const rollback = 99;
+const fullPrice = 200;
+const adaptive = true;
+
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+console.log(screens.toLowerCase().split(""));
+console.log(fullPrice * (rollback / 100));
 
 console.log("любое сообщение");
+console.log(16 ** 0.5); //оказывается так тоже можно
