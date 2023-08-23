ca1c3925af39c7c9fed2e8e3909354b095ece642
Dmitry <dmitri.designer@gmail.com>
2023-08-23 12:41:14 +0300

    lesson16

--- script.js
+++ script.js
@@ -1,53 +1,112 @@
+'use strict'
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+
+    sayHello() {
+        console.log(`Hello my name is ${this.name}`);
+    }
+}
+
+class FrontEndDev extends Person {
+    constructor(name, age, skills = []) {
+        super(name, age)
+        this._skills = skills;
+    }
+    get skills() {
+        return this._skills
+    }
+    set skills(str) {
+        this._skills.push(str);
+    }
+}
+
+const dev = new FrontEndDev('Vlad', 23)
+
+dev.skills = '1 sv'
+dev.skills = '2 sv'
+dev.skills = '3 sv'
+
+console.log(dev);
+
+
+////////////// деструктуризация прямо при передаче обьекта в функцию
+
+const logger = ({ first, second, third }) => {
+    console.log(first);
+    console.log(second);
+    console.log(third);
+}
+
+logger({ first: 'I', third: 'JavaScript', second: 'love' })
+
+// rest и spread
+
+const sum = (a, b, c, ...params) => {
+    console.log(params)
+}
+sum(1, 2, 3, 4, 5, 6, 'args', false)
+
+// ответом будет массив [4,5,6,'args',false]
+console.log('rest и spread');
+
+const sum1 = (...params) => {
+    return params.reduce((sum, num) => sum + num)
+}
+
+console.log(sum1(1, 2, 3, 4, 5, 6))
+
+// spread
+
+const first = [1, 2, 3, 4];
+const second = [5, 6, 7, 8];
+
+console.log(23232, ...first, 121312, ...second);
 
 
 
-function DomElement(selector, height, width, fontSize, bg) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.fontSize = fontSize;
-    this.bg = bg;
+///////////
+
+// Напишите расширения метода прототипа:
+// 1) Два класса, First и Second, Second наследует от First.
+
+// 2) В First есть метод hello - он печатает в консоль "Привет я метод родителя!".
+
+// 3) Нужно написать в Second метод hello, чтоб он сначала вызывал метод hello из First, а потом сразу печатал в консоль "А я наследуемый метод!"
+
+// Проверить, чтобы все работало без ошибок в консоли
+// Запушить задание в репозиторий на github или реализовать на доске CodePen и прикрепить ссылку
 
 
-    this.addDom = function () {
-        if (selector[0] == '.') {
-            let div = document.createElement('div');
-            div.className = selector;
-            div.textContent = "Booooooy";
-            console.log(div);
-            document.body.append(div);
+console.log(`Домашка:`)
 
-            div.style.cssText = `
-            height: ${this.height}px;
-            width: ${this.width};
-            background-color:${this.bg}px;
-            font-size:${this.fontSize}px;
-            `;
+class First {
+    constructor() {
 
-        } else if (selector[0] == '#') {
-
-            let p = document.createElement('p');
-            p.id = selector;
-            p.textContent = "Booooooy";
-            console.log(p);
-            document.body.append(p);
-
-            p.style.cssText = `
-            height: ${this.height}px;
-            width: ${this.width};
-            background-color:${this.bg}px;
-            font-size:${this.fontSize}px;
-            `;
-
-        } else console.log('это хз')
-
-    };
-};
+    }
+    hello() {
+        console.log("Привет я метод родителя!")
+    }
+}
 
 
-let boris = new DomElement('.cooool', 200, 400, 27, 'grey');
 
+class Second extends First {
+    constructor() {
+        super()
+    }
+    hello() {
+        super.hello()
+        console.log("А я наследуемый метод!")
+    }
 
-boris.addDom()
+}
+
+const test = new Second
+
+test.hello()
 
 
